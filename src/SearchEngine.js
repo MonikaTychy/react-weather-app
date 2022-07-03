911972c4148c61f5287cf23f1cc6a1c83f257e6a
MonikaTychy <mo.tychy@gmail.com>
2022-07-03 21:34:36 +0200

    SearchEngine activation 1st step

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchEngine.css';
 
 export default function SearchEngine () {
+
+    const [city, setCity] = useState ("");
+
+    function handleSubmit (event) {
+        event.preventDefault();
+        alert (city);
+    }
+
+    function updateCity (event) {
+      setCity(event.target.value);
+    }
  
     return (      
       <div className='SearchEngine'>
-<form>
+<form onSubmit={handleSubmit}>
  <div className='row'>
   <div className='col-8'>
-   <input type='search' placeholder='Enter a city...' className='form-control' autoFocus />
+   <input type='search' placeholder='Enter a city...' className='form-control' autoFocus onChange={updateCity}/>
   </div>
   <div className='col-4'>
    <button type='submit' className='btn btn-dark w-100'>Search</button>
